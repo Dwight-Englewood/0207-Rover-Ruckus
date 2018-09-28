ce1fa4c75ee75687ac7af0dfbee4db0b030170c4
Wen <waterwasp55555@gmail.com>
2018-09-28 19:43:39 -0400

    Blinkin Testing Program

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LEDMode.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "LEDMode", group = "Teleop")
+public class LEDMode extends OpMode {
+
+    RevBlinkinLedDriver blinkin;
+
+    @Override
+    public void init() {
+        blinkin = hardwareMap.get(RevBlinkinLedDriver.class, "rgbReady");
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+        } else if (gamepad1.b) {
+            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_FOREST_PALETTE);
+        } else if (gamepad1.x) {
+            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_RAINBOW_PALETTE);
+        } else if (gamepad1.y) {
+            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.CONFETTI);
+        }
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
