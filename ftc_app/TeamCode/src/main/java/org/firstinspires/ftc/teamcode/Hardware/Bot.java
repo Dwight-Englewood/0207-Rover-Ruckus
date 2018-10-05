528ba02c9afa48b453d98766f358858a29ebbba6
Burtorustum <burtorustum@gmail.com>
2018-10-05 18:32:50 -0400

    classes for hardware subsystem stuff

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Bot.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+public abstract class Bot {
+}
---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/BotImpl.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+public class BotImpl extends Bot {
+}
