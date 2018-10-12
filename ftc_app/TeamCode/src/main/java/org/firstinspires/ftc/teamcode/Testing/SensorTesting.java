36fbf1bc84235463ec626854d63bc8b0988590d6
Wen <waterwasp55555@gmail.com>
2018-10-12 19:53:05 -0400

    this registered twice for some reason, so disabled it

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/SensorTesting.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/SensorTesting.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Testing;
 import android.graphics.Color;
 
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
@@ -10,7 +11,7 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-
+@Disabled
 @TeleOp(name = "SensorTesting", group = "Teleop")
 public class SensorTesting extends OpMode {
 
