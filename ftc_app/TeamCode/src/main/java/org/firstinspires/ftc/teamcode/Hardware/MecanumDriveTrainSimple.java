abbc9a528e8fb8dd3fab35d21785299f0faf3e79
Burtorustum <burtorustum@gmail.com>
2018-10-12 18:02:16 -0400

    privated a method

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/MecanumDriveTrainSimple.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/MecanumDriveTrainSimple.java
@@ -46,7 +46,7 @@ public class MecanumDriveTrainSimple {
         return new PowerVector4WD(powerMatrix.mult(rotationMatrix(drv.get(2,0)).mult(drv).scale(-Math.sqrt(2)/r)));
     }
 
-    SimpleMatrix rotationMatrix(double theta) {
+    private SimpleMatrix rotationMatrix(double theta) {
         return new SimpleMatrix(new double[][]{{Math.cos(theta), Math.sin(theta), 0}, {-Math.sin(theta), Math.cos(theta), 0}, {0, 0, 1}});
     }
 }
