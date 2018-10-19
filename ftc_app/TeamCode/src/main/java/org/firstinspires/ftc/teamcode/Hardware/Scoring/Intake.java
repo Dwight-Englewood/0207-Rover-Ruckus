5c068ee3ff782771d78d3eca8827e65c6515fe57
Burtorustum <burtorustum@gmail.com>
2018-10-19 19:20:59 -0400

    Implemented Intake

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Scoring/Intake.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Scoring/Intake.java
@@ -1,17 +1,31 @@
 package org.firstinspires.ftc.teamcode.Hardware.Scoring;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
 import org.firstinspires.ftc.teamcode.Hardware.System;
 
 public class Intake implements System {
-    public Intake() {
+    public Intake() {}
 
+    private DcMotor motor;
+    private enum intakeState {
+        INTAKING,
+        OUTTAKING,
+        STOPPED;
     }
+    private intakeState state;
 
     @Override
     public void init(HardwareMap hwMap) {
-
+        motor = hwMap.get(DcMotor.class, "int");
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        state = intakeState.STOPPED;
     }
 
     @Override
@@ -21,11 +35,34 @@ public class Intake implements System {
 
     @Override
     public void reset() {
+        motor.setPower(0);
+        state = intakeState.STOPPED;
+    }
 
+    public void intake() {
+        motor.setPower(1);
+        state = intakeState.INTAKING;
+    }
+
+    public void outtake() {
+        motor.setPower(-1);
+        state = intakeState.OUTTAKING;
     }
 
     @Override
     public String toString() {
-        return "";
+        switch(state) {
+            case INTAKING:
+                return "Intaking";
+
+            case OUTTAKING:
+                return "Outtaking";
+
+            case STOPPED:
+                return "Stopped";
+
+            default:
+                return super.toString();
+        }
     }
 }
