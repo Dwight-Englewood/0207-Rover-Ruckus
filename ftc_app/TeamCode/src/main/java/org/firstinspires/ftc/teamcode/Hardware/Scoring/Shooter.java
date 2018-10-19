2dfd8a0ba91b88e63000594de963284eab340613
Burtorustum <burtorustum@gmail.com>
2018-10-19 17:47:38 -0400

    Added classes for scoring mechanisms

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Scoring/Shooter.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Hardware.Scoring;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Hardware.Subsystem;
+
+public class Shooter implements Subsystem {
+    public Shooter() {
+
+    }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
