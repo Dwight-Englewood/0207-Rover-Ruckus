944311cd218d3fd9b35391c2718705c5bf1878b1
Burtorustum <burtorustum@gmail.com>
2018-10-19 16:39:22 -0400

    Added class for driver-controlled period

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Telebop.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "Telebop", group = "Teleop")
+public class Telebop extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
