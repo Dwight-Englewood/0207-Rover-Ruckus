8ec90df80d69ac43922656d51d898e708852e6e8
Burtorustum <burtorustum@gmail.com>
2018-10-19 16:38:52 -0400

    Added class files for four auton start positions

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedGold.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Autonomous(name = "RedGold", group = "Auton")
+public class RedGold extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
+
