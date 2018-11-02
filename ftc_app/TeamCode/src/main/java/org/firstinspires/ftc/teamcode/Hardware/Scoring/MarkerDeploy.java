6d3089ac38c06bfa351ca9aaade00c56e34b5c8e
Burtorustum <burtorustum@gmail.com>
2018-11-02 15:25:10 -0400

    Implemented marker deploy mechanism

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Scoring/MarkerDeploy.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Scoring/MarkerDeploy.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Hardware.Scoring;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Hardware.State;
 import org.firstinspires.ftc.teamcode.Hardware.Subsystem;
@@ -10,28 +11,54 @@ public class MarkerDeploy implements Subsystem {
 
     }
 
+    private Servo servo;
+
+    private enum markerState implements State {
+        HOLDING("Holding"),
+        DROPPING("Dropping");
+
+
+        private String str;
+        markerState(String str) {
+            this.str = str;
+        }
+
+        @Override
+        public String getStateVal() {
+            return str;
+        }
+    }
+    private markerState state;
+
     @Override
     public void init(HardwareMap hwMap) {
-
+        servo = hwMap.get(Servo.class, "mark");
+        this.raise();
     }
 
     @Override
     public void start() {
-
+        this.raise();
     }
 
     @Override
     public void reset() {
-
+        this.raise();
     }
 
     @Override
-    public void stop() {
+    public void stop() {}
 
+    public void drop() {
+        servo.setPosition(0);
+    }
+
+    public void raise() {
+        servo.setPosition(1);
     }
 
     @Override
     public State getState() {
-        return null;
+        return state;
     }
 }
