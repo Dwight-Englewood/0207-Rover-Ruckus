bbc04c9ea30fcb1f21f0df7844e30536d94c40e1
Burtorustum <burtorustum@gmail.com>
2018-11-03 08:21:54 -0400

    Enabled Sensors subsystem properly

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Bot.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Bot.java
@@ -3,10 +3,9 @@ package org.firstinspires.ftc.teamcode.Hardware;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Hardware.Drivetrain.MecanumDriveTrain;
-import org.firstinspires.ftc.teamcode.Hardware.Scoring.Intake;
 import org.firstinspires.ftc.teamcode.Hardware.Scoring.Lift;
 import org.firstinspires.ftc.teamcode.Hardware.Scoring.MarkerDeploy;
-import org.firstinspires.ftc.teamcode.Hardware.Scoring.Shooter;
+import org.firstinspires.ftc.teamcode.Hardware.Sensors.Sensors;
 
 import java.lang.reflect.Field;
 
@@ -19,7 +18,7 @@ public class Bot {
     public Lift lift = new Lift();
     //public Shooter shooter = new Shooter();
     public MarkerDeploy markerDeploy = new MarkerDeploy();
-    //public Sensors sensors = new Sensors();
+    public Sensors sensors = new Sensors();
 
 
     public void init(HardwareMap hwm) {
