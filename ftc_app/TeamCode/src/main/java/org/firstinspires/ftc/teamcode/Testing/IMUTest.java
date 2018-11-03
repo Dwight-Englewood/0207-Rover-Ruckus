057a6ee88c2adcbc221a23ceddb16a76422e86d0
Burtorustum <burtorustum@gmail.com>
2018-11-03 11:49:11 -0400

    Tests for IMU and Lift

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/IMUTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.teamcode.Hardware.Bot;
+
+
+@TeleOp(name = "IMUTest", group = "Teleop")
+public class IMUTest extends OpMode {
+
+    Bot robot = new Bot();
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        robot.start();
+    }
+
+    @Override
+    public void loop() {
+        int gyroVal = (int)robot.sensors.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        robot.driveTrain.gyroCorrect(0,1, gyroVal, .05, .4);
+
+        telemetry.addData("heading", gyroVal);
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        robot.stop();
+    }
+}
---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/LiftTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Hardware.Bot;
+
+
+@TeleOp(name = "LiftTest", group = "Teleop")
+public class LiftTest extends OpMode {
+
+    Bot robot = new Bot();
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        robot.start();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("newyears?", robot.lift.newYears());
+    }
+
+    @Override
+    public void stop() {
+        robot.stop();
+    }
+}
