776c472ee9ddab2421b3ea022ed0896a4965d673
Burtorustum <burtorustum@gmail.com>
2018-11-20 11:39:48 -0500

    fixed TFWrapper Enum fr this time

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Sensors/TFWrapper.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Sensors/TFWrapper.java
@@ -25,20 +25,26 @@ public class TFWrapper implements Subsystem {
 
     public TFWrapper() {}
 
-    private enum TFState implements State {
-        LEFT("Left"),
-        RIGHT("Right"),
-        CENTER("Center");
+    public enum TFState implements State {
+        LEFT("Left", 0, 0), //temp vals
+        RIGHT("Right", 0, 0), //temp vals
+        CENTER("Center", 0, 0); //temp vals
 
         private String str;
-        TFState(String str) {
+        private int craterAng;
+        private int depotAng;
+        TFState(String str, int craterAng, int depotAng) {
             this.str = str;
+            this.craterAng = craterAng;
+            this.depotAng = depotAng;
         }
 
         @Override
         public String getStateVal() {
             return str;
         }
+        public int getCraterAng() {return craterAng;}
+        public int getDepotAng() {return depotAng;}
     }
     private TFState state;
 
