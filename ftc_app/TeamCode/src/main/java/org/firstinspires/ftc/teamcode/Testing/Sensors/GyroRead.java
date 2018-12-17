a3c2ec0a451e995da0e74ad56cefe8efde9c732d
Burtorustum <burtorustum@gmail.com>
2018-12-17 08:08:02 -0500

    Disabled GyroRead

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Sensors/GyroRead.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Sensors/GyroRead.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Testing.Sensors;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.teamcode.Hardware.Bot;
 
 
 @Autonomous(name = "GyroRead", group = "Testing")
-//@Disabled
+@Disabled
 public class GyroRead extends OpMode {
 
     Bot robot = new Bot(true);
