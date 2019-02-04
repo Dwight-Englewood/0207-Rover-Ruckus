4f354b476df3c339436f762ab80bd23f7ad80b12
Wen <waterwasp55555@gmail.com>
2019-02-03 20:11:02 -0500

    Tensorflow tuning teleop

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/GoldDetectorWrapper.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/GoldDetectorWrapper.java
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.teamcode.Testing;
 
 import org.corningrobotics.enderbots.endercv.OpenCVPipeline;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
@@ -15,17 +13,17 @@ import java.util.List;
  * Created by guinea on 10/5/17.
  * -------------------------------------------------------------------------------------
  * Copyright (c) 2018 FTC Team 5484 Enderbots
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,8 +31,8 @@ import java.util.List;
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
- *
+ * <p>
+ * <p>
  * By downloading, copying, installing or using the software you agree to this license.
  * If you do not agree to this license, do not download, install,
  * copy or use the software.
@@ -44,19 +42,16 @@ import java.util.List;
  * robotics OpenCV applications.
  */
 
-public class ExampleBlueVision extends OpenCVPipeline {
-    private boolean showContours = true;
+public class GoldDetectorWrapper extends OpenCVPipeline {
+    public ImageView imageView = ImageView.RGB;
+    public GoldDetectorPipeline grip = new GoldDetectorPipeline();
     // To keep it such that we don't have to instantiate a new Mat every call to processFrame,
     // we declare the Mats up here and reuse them. This is easier on the garbage collector.
     private Mat hsv = new Mat();
     private Mat thresholded = new Mat();
-
     // this is just here so we can expose it later thru getContours.
     private List<MatOfPoint> contours = new ArrayList<>();
 
-    public synchronized void setShowCountours(boolean enabled) {
-        showContours = enabled;
-    }
     public synchronized List<MatOfPoint> getContours() {
         return contours;
     }
@@ -64,31 +59,27 @@ public class ExampleBlueVision extends OpenCVPipeline {
     // This is called every camera frame.
     @Override
     public Mat processFrame(Mat rgba, Mat gray) {
-        // First, we change the colorspace from RGBA to HSV, which is usually better for color
-        Imgproc.cvtColor(rgba, hsv, Imgproc.COLOR_RGB2HSV, 3);
-        // Then, we threshold our hsv image so that we get a black/white binary image where white
-        // is the blues listed in the specified range of values
-        // you can use a program like WPILib GRIP to find these values, or just play around.
-        Core.inRange(hsv, new Scalar(90, 128, 30), new Scalar(170, 255, 255), thresholded);
-
-        // we blur the thresholded image to remove noise
-        // there are other types of blur like box blur or gaussian which can be explored.
-        Imgproc.blur(thresholded, thresholded, new Size(3, 3));
-
-        // create a list to hold our contours.
-        // Conceptually, there is going to be a single contour for the outline of every blue object
-        // that we can find. We can iterate over them to find objects of interest.
-        // the Imgproc module has many functions to analyze individual contours by their area, avg position, etc.
-        contours = new ArrayList<>();
-        // this function fills our contours variable with the outlines of blue objects we found
-        Imgproc.findContours(thresholded, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-        // Then we display our nice little binary threshold on screen
-        if (showContours) {
-            // this draws the outlines of the blue contours over our original image.
-            // they are highlighted in green.
-            Imgproc.drawContours(rgba, contours, -1, new Scalar(0, 255, 0), 2, 8);
+        grip.process(rgba);
+        switch (imageView) {
+            case RGB:
+                return grip.cvCvtcolorOutput();
+            case HSV:
+                return grip.hsvThresholdOutput();
+            case BLUR:
+                return grip.blurOutput();
+            case CONTOUR:
+                Imgproc.drawContours(rgba, grip.findContoursOutput(), -1, new Scalar(255, 255, 255), 8);
+                return rgba;
+            case HULL:
+                Imgproc.drawContours(rgba, grip.convexHullsOutput(), -1, new Scalar(255, 255, 255), 8);
+                return rgba;
+            default:
+                return rgba;
         }
 
-        return rgba; // display the image seen by the camera
+    }
+
+    public enum ImageView {
+        RGB, HSV, BLUR, CONTOUR, HULL
     }
 }
