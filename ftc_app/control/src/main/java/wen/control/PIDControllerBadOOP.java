2685cca5518688dee103592acc0b50b8bb438ac2
Wen <waterwasp55555@gmail.com>
2019-02-08 22:35:52 -0500

    more public than my local library

---
+++ ftc_app/control/src/main/java/wen/control/PIDControllerBadOOP.java
@@ -0,0 +1,27 @@
+package wen.control;
+
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class PIDControllerBadOOP extends PIDController {
+
+
+    public PIDControllerBadOOP(double pGain, double iGain, double dGain) {
+        super(pGain, iGain, dGain);
+    }
+
+    public PIDControllerBadOOP(double pGain, double iGain, double dGain, double goal) {
+        super(pGain, iGain, dGain, goal);
+    }
+
+    public void updateError(double currentPosition) {
+        super.updateError(currentPosition);
+    }
+
+    public double correction() {
+        return super.correction();
+    }
+
+
+}
