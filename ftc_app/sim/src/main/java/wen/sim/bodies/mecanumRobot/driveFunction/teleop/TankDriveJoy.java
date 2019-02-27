f579f7611f76ee13d4ded0d5bf6ee579bc038686
Wen <waterwasp55555@gmail.com>
2019-02-27 14:03:46 -0500

    sim joystick checks

--- ftc_app/sim/src/main/java/wen/sim/bodies/mecanumRobot/driveFunction/teleop/TankDriveJoy.java
+++ ftc_app/sim/src/main/java/wen/sim/bodies/mecanumRobot/driveFunction/teleop/TankDriveJoy.java
@@ -3,7 +3,6 @@ package wen.sim.bodies.mecanumRobot.driveFunction.teleop;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
-import wen.control.function.Coordinate;
 import wen.sim.bodies.mecanumRobot.MecanumRobot;
 import wen.sim.bodies.mecanumRobot.driveFunction.MecanumDriveMode;
 
@@ -14,38 +13,42 @@ import static org.lwjgl.glfw.GLFW.glfwGetJoystickButtons;
 public class TankDriveJoy implements MecanumDriveMode {
 
     public void updateWheelPower(long window, MecanumRobot bot) {
-        FloatBuffer joysticks = glfwGetJoystickAxes(GLFW_JOYSTICK_1);
-        ByteBuffer buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1);
+        try {
+            FloatBuffer joysticks = glfwGetJoystickAxes(GLFW_JOYSTICK_1);
+            ByteBuffer buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1);
 
-        double leftStickY = -1 * joysticks.get(1);
+            double leftStickY = -1 * joysticks.get(1);
 
-        double rightStickY = -1 * joysticks.get(4);
+            double rightStickY = -1 * joysticks.get(4);
 
-        double deadzone = .3f;
-        if (leftStickY < deadzone && leftStickY > -deadzone) {
-            leftStickY = 0;
+            double deadzone = .3f;
+            if (leftStickY < deadzone && leftStickY > -deadzone) {
+                leftStickY = 0;
+            }
+            if (rightStickY < deadzone && rightStickY > -deadzone) {
+                rightStickY = 0;
+            }
+
+            bot.wheelFL = leftStickY;
+            bot.wheelBL = leftStickY;
+            bot.wheelFR = rightStickY;
+            bot.wheelBR = rightStickY;
+
+            if (buttons.get(7) == 1) {
+                bot.wheelFL = 1;
+                bot.wheelBL = -1;
+                bot.wheelFR = -1;
+                bot.wheelBR = 1;
+            } else if (buttons.get(6) == 1) {
+                bot.wheelFL = -1;
+                bot.wheelBL = 1;
+                bot.wheelFR = 1;
+                bot.wheelBR = -1;
+            }
+
+        } catch (NullPointerException e) {
+
         }
-        if (rightStickY < deadzone && rightStickY > -deadzone) {
-            rightStickY = 0;
-        }
-
-        bot.wheelFL = leftStickY;
-        bot.wheelBL = leftStickY;
-        bot.wheelFR = rightStickY;
-        bot.wheelBR = rightStickY;
-
-        if (buttons.get(7) == 1) {
-            bot.wheelFL = 1;
-            bot.wheelBL = -1;
-            bot.wheelFR = -1;
-            bot.wheelBR = 1;
-        } else if (buttons.get(6) == 1) {
-            bot.wheelFL = -1;
-            bot.wheelBL = 1;
-            bot.wheelFR = 1;
-            bot.wheelBR = -1;
-        }
-
     }
 
     @Override
